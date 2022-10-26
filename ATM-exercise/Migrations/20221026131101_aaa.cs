155d350b3e2ac1a7f723550dfc0e9d53c58d9b42
BrunoGoretti <104521859+BrunoGoretti@users.noreply.github.com>
2022-10-26 17:12:31 +0400

    Added server configuration

---
+++ ATM-exercise/Migrations/20221026131101_aaa.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ATM_exercise.Migrations
+{
+    public partial class aaa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ATM",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsCardInserted = table.Column<bool>(type: "bit", nullable: false),
+                    IsOperatorMode = table.Column<bool>(type: "bit", nullable: false),
+                    ATMmachineBalance = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    WithdrawalFeeAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    WithdrawalDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ATM", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PinCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonSurname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CardId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserBalance = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ATM");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
